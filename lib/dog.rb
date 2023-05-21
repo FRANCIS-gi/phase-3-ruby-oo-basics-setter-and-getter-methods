925501810340d3063498e9ffa535816e66ffc0f5
Francis <fgkiron@gmail.com>
2023-05-21 15:32:01 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end    
+
 
 end
+kamau = Dog.new
+kamau.name = "Kamau"
+
+mastiff = Dog.new
+mastiff.name = "Mastiff"
